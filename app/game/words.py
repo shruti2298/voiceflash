c2df819d8e13807efbf2b7392fc931c3be932f8d
shrutipatolia <shruti.patolia@dream11.com>
2026-07-02 00:32:29 +0530

    feat: seeded word pool

---
+++ app/game/words.py
@@ -0,0 +1,7 @@
+# Simple, phonetically-distinct, TTS/STT-friendly concrete nouns.
+WORDS = [
+    "apple", "tiger", "river", "guitar", "planet", "candle", "rocket", "garden",
+    "monkey", "pencil", "orange", "dragon", "castle", "silver", "yellow", "window",
+    "mountain", "diamond", "pumpkin", "butterfly", "helicopter", "umbrella",
+    "elephant", "chocolate", "computer", "sunflower",
+]
